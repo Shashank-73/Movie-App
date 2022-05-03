9b1e1391f93e7f1427c74dee65879c2725ba81ff
Shashank-73 <shashank220076@gmail.com>
2022-05-03 22:28:38 +0530

    bootstrap added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import FavoriteIcon from '@mui/icons-material/FavoriteIcon';
 import './index.css'
 import { Typography } from '@mui/material';
 import {Link} from "react-router-dom"
@@ -10,7 +9,7 @@ export default class Navbar extends Component {
         <Link to="/" style={{ textDecoration: 'none', color: '#d47c50'}}><h1>MYmovie.com</h1></Link>
         <Link to="/favorites">
           <Typography variant='h2'>
-            <FavoriteIcon />
+            Favorite
           </Typography>
         </Link>
       </div>
